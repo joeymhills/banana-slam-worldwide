fe2d8524a064ca854c0495bb1a6ca4662ed12db1
Joey Hills <joeymhills@gmail.com>
2023-08-16 22:22:15 -0400

    dnwjk

--- bananaslam/src/app/page.tsx
+++ bananaslam/src/app/page.tsx
@@ -2,7 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-  <div className="bg-yellow"></div>
+  <div className="bg-white container min-h-screen">
+      <h1 className='w-full text-yellow-500 flex flex-col justify-center items-center pt-6 font-bold text-5xl'>Banana SLAM!</h1>
+  </div>
 
   )
 }
