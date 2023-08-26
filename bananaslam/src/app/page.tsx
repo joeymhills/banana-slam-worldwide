ac8ad1ff0b8e2c49792f239206f44c6d623e80ca
Joey Hills <joeymhills@gmail.com>
2023-08-25 23:22:49 -0400

    ea

--- bananaslam/src/app/page.tsx
+++ bananaslam/src/app/page.tsx
@@ -1,9 +1,34 @@
+"use client"
+
+import axios from 'axios';
 import Image from 'next/image'
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [albums, setAlbums] = useState("")
+
+  async function goGet(){
+
+    const response = await fetch("https://fiber-test-production.up.railway.app/albums", {
+    method: "GET",
+    mode: "no-cors", 
+    })
+  
+    if (response?.ok){
+      setAlbums(response)
+    }
+    else{
+      console.log("error")
+    }
+  }
+  
+  goGet();
+
   return (
   <div className="bg-black w-full min-h-screen">
       <h1 className='w-full text-yellow-500 flex flex-col justify-center items-center pt-6 font-bold text-5xl'>Banana SLAM!</h1>
+      <div>{albums}</div>
   </div> //white background with yellow text (centered)
   )
 }
